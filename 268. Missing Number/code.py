f942351d23c52ece8c96ac363b6bb6217689fa44
anjuvilashni <anjuvilashni.n@gmail.com>
2024-03-10 17:05:21 -0400

    268. Missing Number

---
+++ 268. Missing Number/code.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(0, len(nums)+1):
+            if i not in nums:
+                return i
+
+if __name__ == "__main__":
+    a = Solution()
+    nums = [3,0,1]
+    print(a.missingNumber(nums))  
